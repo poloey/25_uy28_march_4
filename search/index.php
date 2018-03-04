b76a218c7d8f950684ddb369d0de47ac5c173b4f
= <=>
2018-03-04 16:22:58 +0600

    added material css search

--- search/index.php
+++ search/index.php
@@ -1,61 +1,92 @@
 <?php 
-$con = new PDO('mysql:dbname=feni;host=localhost', 'root', '');
+$con = new PDO('mysql:host=localhost;dbname=feni', 'root', '');
 
 $statement = $con->prepare('select * from teachers');
 if (isset($_GET['q'])) {
   $q = $_GET['q'];
-  $statement = $con->prepare("
-    select * from teachers where
-    name like '%$q%' or
-    email like '%$q%'
-
-    ");
+  $type = $_GET['type'];
+  if ($type === 'email') {
+    $statement = $con->prepare("select * from teachers where email like '%$q%'");
+  } else {
+    $statement = $con->prepare("select * from teachers where name like '%$q%'");
+  }
 }
 $statement->execute();
-$teachers = $statement->fetchAll();
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title>Document</title>
-  <link rel="stylesheet" href="bootstrap.min.css">
-</head>
-<body>
+$teachers = $statement->fetchAll(PDO::FETCH_OBJ);
+ ?>
+ <!DOCTYPE html>
+  <html>
+    <head>
+      <!--Import Google Icon Font-->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-alpha.4/css/materialize.min.css">
 
-  <div class="container mt-5">
-    <div class="card">
-      <div class="card-header">
-        <h2>
-          <a href="index.php">All teachers</a>
-        </h2> 
-      </div>
-      <div class="card-body">
-        <form class="col-md-6 mx-auto">
-          <div class="form-group">
-            <div class="input-group">
-              <input type="text" class="form-control" name="q">
-              <div class="input-group-append">
-                <button type="submit" class="btn btn-info">Search</button>
-              </div>
+            
+
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </head>
+
+    <body>
+      <div class="container">
+        <div class="card">
+          <div class="card-content">
+            <div class="card-title">
+              <h2>All teachers</h2>
             </div>
+            <form>
+              <div class="row">
+                <div class="input-field col s12">
+                  <label for="search" class="">Search</label>
+                  <input type="text" name="q" id="search" class="">
+                </div>
+              </div>
+              <div class="row">
+                <div class="input-field col s12">
+                  <select name="type">
+                    <option value="name">Name</option>
+                    <option value="email">Email</option>
+                  </select>
+                  <label>Type</label>
+                </div>
+              </div>
+              <div class="row">
+                <div class="input-field col s12">
+                  <button type="submit" class="btn red">search</button>
+                </div>
+              </div>
+              </div>
+            </form>
+            <table class="table table-bordered">
+              <tr>
+                <th>Name</th>
+                <th>Eamil</th>
+              </tr>
+              <?php foreach ($teachers as $teacher): ?>
+                <tr>
+                  <td><?php echo $teacher->name ?></td>
+                  <td><?php echo $teacher->email ?></td>
+                </tr>
+              <?php endforeach ?>
+            </table>
           </div>
-        </form>
-        <table class="table table-bordered">
-          <tr>
-            <th>Name</th>
-            <th>Email</th>
-          </tr>
-          <?php foreach ($teachers as $teacher): ?>
-            <tr>
-              <td><?php echo $teacher['name'] ?></td>
-              <td><?php echo $teacher['email'] ?></td>
-            </tr>
-          <?php endforeach ?>
-        </table>
+        </div>
       </div>
-    </div>
-  </div>
-  
-</body>
-</html>
+
+      <!--JavaScript at end of body for optimized loading-->
+    <!-- Compiled and minified JavaScript -->
+    <script
+  src="https://code.jquery.com/jquery-3.3.1.js"
+  integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
+  crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-alpha.4/js/materialize.min.js"></script>
+    <script>
+      
+  $(document).ready(function(){
+    $('select').formSelect();
+  });
+        
+    </script>
+    </body>
+  </html>
---
+++ search/old.php
@@ -0,0 +1,61 @@
+<?php 
+$con = new PDO('mysql:dbname=feni;host=localhost', 'root', '');
+
+$statement = $con->prepare('select * from teachers');
+if (isset($_GET['q'])) {
+  $q = $_GET['q'];
+  $statement = $con->prepare("
+    select * from teachers where
+    name like '%$q%' or
+    email like '%$q%'
+
+    ");
+}
+$statement->execute();
+$teachers = $statement->fetchAll();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+  <link rel="stylesheet" href="bootstrap.min.css">
+</head>
+<body>
+
+  <div class="container mt-5">
+    <div class="card">
+      <div class="card-header">
+        <h2>
+          <a href="index.php">All teachers</a>
+        </h2> 
+      </div>
+      <div class="card-body">
+        <form class="col-md-6 mx-auto">
+          <div class="form-group">
+            <div class="input-group">
+              <input type="text" class="form-control" name="q">
+              <div class="input-group-append">
+                <button type="submit" class="btn btn-info">Search</button>
+              </div>
+            </div>
+          </div>
+        </form>
+        <table class="table table-bordered">
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+          <?php foreach ($teachers as $teacher): ?>
+            <tr>
+              <td><?php echo $teacher['name'] ?></td>
+              <td><?php echo $teacher['email'] ?></td>
+            </tr>
+          <?php endforeach ?>
+        </table>
+      </div>
+    </div>
+  </div>
+  
+</body>
+</html>
